886ea1298a4b5abc1a5c85726de8cebf4c2d752f
Tim Davis <davistim523@gmail.com>
2026-02-23 02:51:27 -0600

    Module 3 completed baseline

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,6 +1,9 @@
-/* GET travel page */
+var fs = require('fs');
+
+var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+
 const travel = (req, res) => {
-res.render('travel', { title: 'Travlr Getaways' });
+res.render('travel', { title: 'Travlr Getaways', trips: trips });
 };
 
 module.exports = {
